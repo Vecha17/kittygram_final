f5fe3623637efa3b1455bc4fa77633056f5cdfe6
Алексей Попов <vechanka@yandex.ru>
2024-04-07 17:19:52 +0300

    07.04.2024 ver_4.2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,13 @@
 # flake8: noqa
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
